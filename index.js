bce33ca697cea8d70d0fa06e65d84458841bb7fa
Rahul Girgal <rahul4girgal@gmail.com>
2023-12-12 13:17:08 +0530

    package .json

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const book = require("../server/books");
+const book = require('./server/books');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
